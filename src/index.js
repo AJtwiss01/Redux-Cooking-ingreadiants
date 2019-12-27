b22829206a29e72bcf50eefc6ca150272a56518a
AJ Twiss <twissaj01@gmail.com>
2019-12-26 21:00:15 -0800

    using jQuery to load ui

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import "./styles.css";
 import store from "./store";
+import loadUI from "./ui";
 import { addRecipe } from "./actions/recipes";
 import { addIngredient } from "./actions/ingredients";
 
 document.getElementById("app").innerHTML = `
-<h1>Basic Example of Redux</h1>
-<h2> Counter in Vanilla JS </h2>
-<div>
-  Counter:
-  <span id='counter'></span>
-</div>
 
-<button id='inc'>Increment</button>
-<button id='dec'>Decrement</button>
 
 `;
+
+loadUI();
 
 store.dispatch(addRecipe("Pancakes"));
 store.dispatch(addIngredient("Pancakes", "Egg", 3));
